e73abf701df9c49544ccfe2f19598ac58cf7d759
wucong <1875486458@qq.com>
2019-09-07 11:03:06 +0800

    try to just use tokio:fs

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,9 @@ use std::{fs::{self, File}, path::*, io::SeekFrom, io::prelude::*};
 use std::ffi::OsStr;
 use std::collections::VecDeque;
 use log::*;
-use tokio::prelude::Future;
+use tokio::prelude::{Future, AsyncRead};
+use tokio::io;
+use tokio::codec::Decoder;
 
 pub struct Tail {
     buff: Vec<u8>,
@@ -26,7 +28,7 @@ fn read_file(path: &Path, from: usize, buf: &mut Vec<u8>) -> Result<usize, Error
     return Ok(file.read(buf)? as usize);
 }
 
-impl<'a> Tail {
+impl Tail {
     pub fn new(path: &dyn AsRef<OsStr>) -> Result<Self, Error> {
         let mut inotify = Inotify::init()?;
         let path = path.as_ref();
@@ -79,13 +81,12 @@ impl Stream for Tail {
         info!("poll");
         if let Some(line) = self.lines.pop_front() {
             info!("just pop");
-
             return Ok(Async::Ready(Some(line)));
         }
         loop {
             info!("loop");
-            match self.inotify_event_stream.poll() {
-                Ok(Async::Ready(Some(event))) => {
+            match try_ready!(self.inotify_event_stream.poll()) {
+                Some(event) => {
                     info!("inotify_event_stream Ready {} {:?}", now(), event.mask);
                     if event.mask == EventMask::DELETE || event.mask == EventMask::DELETE_SELF {
                         return Ok(Async::Ready(None));
@@ -99,21 +100,15 @@ impl Stream for Tail {
                         return Ok(Async::Ready(Some(line)));
                     }
                 }
-                Ok(Async::Ready(None)) => {
+                None => {
                     info!("Async::Ready(None)");
                     return Ok(Async::Ready(None));
-                }
-                Ok(Async::NotReady) => {
-                    info!("Async::NotReady");
-                    return Ok(Async::NotReady);
-                }
-                Err(e) => {
-                    return Err(failure::err_msg(format!("inotify_event_stream err {:?}", e)));
                 }
             }
         }
     }
 }
+
 
 fn now() -> String {
     use std::time::{SystemTime, UNIX_EPOCH};
@@ -158,14 +153,87 @@ impl Stream for FileStream {
                     let mut f = tokio::fs::File::open("./data")
                         .and_then(|mut f| f.seek(SeekFrom::Start(self.offset as u64)))
                         .and_then(|(f, offset)| tokio::io::read_to_end(f, vec![]));
-                    let (f,buff) =  try_ready!(f.poll());
-                    return Ok(Async::Ready(Some(buff)))
+                    let (f, buff) = try_ready!(f.poll());
+                    return Ok(Async::Ready(Some(buff)));
                 }
                 None => {
                     return Ok(Async::Ready(None));
                 }
             }
         }
+    }
+}
+
+
+struct TailF {
+    file: PathBuf,
+}
+
+impl TailF {
+    pub fn new(path: &dyn AsRef<OsStr>) -> Result<Self, Error> {
+        let path = path.as_ref();
+        Ok(Self {
+            file: PathBuf::from(path)
+        })
+    }
+}
+
+//impl Stream for TailF {
+//    type Item = String;
+//    type Error = Error;
+//
+//    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+//    }
+//}
+
+pub struct LinesCodec {
+    // Stored index of the next index to examine for a `\n` character.
+    // This is used to optimize searching.
+    // For example, if `decode` was called with `abc`, it would hold `3`,
+    // because that is the next index to examine.
+    // The next time `decode` is called with `abcde\n`, the method will
+    // only look at `de\n` before returning.
+    next_index: usize,
+}
+
+impl LinesCodec {
+    fn decode(&mut self, buf: &mut Vec<u8>) -> Result<Option<String>, io::Error> {
+        Ok(None)
+//        // Look for a byte with the value '\n' in buf. Start searching from the search start index.
+//        if let Some(newline_offset) = buf[self.next_index..].iter().position(|b| *b == b'\n')
+//        {
+//            // Found a '\n' in the string.
+//
+//            // The index of the '\n' is at the sum of the start position + the offset found.
+//            let newline_index = newline_offset + self.next_index;
+//
+//            // Split the buffer at the index of the '\n' + 1 to include the '\n'.
+//            // `split_to` returns a new buffer with the contents up to the index.
+//            // The buffer on which `split_to` is called will now start at this index.
+//            let line = buf.split_to(newline_index + 1);
+//
+//            // Trim the `\n` from the buffer because it's part of the protocol,
+//            // not the data.
+//            let line = &line[..line.len() - 1];
+//
+//            // Convert the bytes to a string and panic if the bytes are not valid utf-8.
+//            let line = std::str::from_utf8(&line).expect("invalid utf8 data");
+//
+//            // Set the search start index back to 0.
+//            self.next_index = 0;
+//
+//            // Return Ok(Some(...)) to signal that a full frame has been produced.
+//            Ok(Some(line.to_string()))
+//        } else {
+//            // '\n' not found in the string.
+//
+//            // Tell the next call to start searching after the current length of the buffer
+//            // since all of it was scanned and no '\n' was found.
+//            self.next_index = buf.len();
+//
+//            // Ok(None) signifies that more data is needed to produce a full frame.
+//            Ok(None)
+//        }
     }
 }
 
@@ -182,20 +250,22 @@ mod tests {
         let _ = TermLogger::init(LevelFilter::Debug, log_config, TerminalMode::Mixed);
     }
 
-    fn append_file(file:&'static str) {
-        let total_count = 10;
+    fn append_file(file: &'static str) {
+        let total_count = 1000;
         let log_path = Path::new(file);
         let _ = fs::remove_file(log_path);
         let mut file = fs::File::create(log_path).unwrap();
-        let long_content = "1".repeat(1024 * 100);
+        let long_content = "1".repeat(3);
         let _ = thread::spawn(move || {
             for i in 0..total_count {
+                info!("{}/{}",i,total_count);
                 let now = now();
                 let data = format!("{} {} {}\n", now, i, long_content);
                 info!("write data {} {}", now, i);
                 let _ = file.write_all(data.as_bytes());
                 thread::sleep(Duration::from_micros(1000));
             }
+            info!("over");
             thread::sleep(Duration::from_secs(1));
 
             let _ = fs::remove_file(log_path);
@@ -208,17 +278,33 @@ mod tests {
         }
         return r - l;
     }
+
+    #[test]
+    fn test_simple() {
+        init_log();
+        append_file("./data");
+        tokio::run(futures::lazy(|| {
+            tokio::fs::File::open(Path::new("./data"))
+                .and_then(|f| {
+                    tokio::codec::Framed::new(f, tokio::codec::LinesCodec::new()).for_each(|line| {
+                        info!("line {} {}", now(), line);
+                        Ok(())
+                    })
+                }).map_err(|e|()).map(|_|())
+        }));
+    }
+
     #[test]
     fn test_file_stream() {
         init_log();
         append_file("./data");
         info!("test_file_stream");
         let f = FileStream::new(&Path::new("./data")).unwrap();
-        tokio::run(futures::lazy(||{
+        tokio::run(futures::lazy(|| {
             tokio::spawn(f.for_each(|buf| {
-                println!("buf len {}",buf.len());
+                println!("buf len {}", buf.len());
                 Ok(())
-            }).map_err(|_|()))
+            }).map_err(|_| ()))
         }))
     }
 
